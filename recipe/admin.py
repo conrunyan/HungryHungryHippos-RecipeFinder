7cc2dc57d3b18e4a7f4945001d411bcb3f82294f
camilne <cameronm213@gmail.com>
2018-02-15 12:31:04 -0700

    Added recipe models

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'difficulty', 'time', 'creation_date')
+
+admin.site.register(models.Recipe, RecipeAdmin)
+admin.site.register(models.Ingredient)
+admin.site.register(models.RecipeIngredient)
+admin.site.register(models.Appliance)
+admin.site.register(models.Comment)
+admin.site.register(models.Group)
--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Group(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+class Ingredient(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+class Appliance(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=50)
+    summary = models.CharField(max_length=280, null=True, blank=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    DIFFICULTY_CHOICES = (
+        ('E', 'Easy'),
+        ('M', 'Medium'),
+        ('D', 'Difficult')
+    )
+    difficulty = models.CharField(max_length=1, null=True, choices=DIFFICULTY_CHOICES)
+    time = models.IntegerField(null=True, blank=True)
+    image_url = models.CharField(max_length=200, null=True, blank=True)
+    rating = models.FloatField(null=True, blank=True)
+    instructions = models.TextField()
+    creation_date = models.DateField(auto_now_add=True)
+    ingredients = models.ManyToManyField(Ingredient, through='RecipeIngredient')
+    appliances = models.ManyToManyField(Appliance)
+
+    def __str__(self):
+        return self.title
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    amount = models.CharField(max_length=10)
+    unit = models.CharField(max_length=10, null=True, blank=True)
+
+class Comment(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    content = models.CharField(max_length=500)
+
+    def __str__(self):
+        return "%s : %s".format(self.user, self.recipe[:30])
