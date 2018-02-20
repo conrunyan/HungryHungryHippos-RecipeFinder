b4e36cce42fbae32a9ddf7c0ea94b08db0d1085d
Kayson <KaysonBarwick@hotmail.com>
2018-02-20 16:06:43 -0700

    added error message and handling for user already existing and added a creation confirmation page.

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 
@@ -10,14 +10,25 @@ def login(request):
 
 def register(request):
 	if request.method == 'POST':
-		message = ""
 		form = CreateUserForm(request.POST)
+
 		if form.is_valid():
-			if(form.cleaned_data['password'] == form.cleaned_data['password_verify']):
-				user = User.objects.create_user(form.cleaned_data['user_name'], form.cleaned_data['email'], form.cleaned_data['password'])
-				message = 'Account creaded successfully'
-			else:
+			user_name = form.cleaned_data['user_name']
+			email = form.cleaned_data['email']
+			password = form.cleaned_data['password']
+			password_verify = form.cleaned_data['password_verify']
+
+			if(password != password_verify):
 				form.add_error('password_verify', 'Passwords did not match')
+
+			if(User.objects.filter(username=user_name)):
+				form.add_error('user_name', 'That user already exists!')
+
+			if(form.is_valid()):
+				User.objects.create_user(user_name, email, password)
+				success_context = { 'user_name' : user_name, 'email' : email}
+				return HttpResponse(render(request, 'accounts/register_successful.html', success_context))
+
 	else:
 		form = CreateUserForm()
 	
@@ -27,3 +38,6 @@ def register(request):
 def logout(request):
 	context = { }
 	return HttpResponse(render(request, 'accounts/logout.html', context))
+
+
+
