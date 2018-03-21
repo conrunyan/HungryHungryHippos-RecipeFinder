e106472ac26ae5e8115b1874d3bd6930d728cafe
Aylee <ayleeandersen@gmail.com>
2018-03-20 22:05:07 -0600

    Set up the index so that it includes an html page to contain the ingredients menu

--- recipe/static/recipe/slidefunctionality.js
+++ recipe/static/recipe/slidefunctionality.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var slideout = new Slideout({
     'panel': document.getElementById('panel'),
     'menu': document.getElementById('menu'),
-    'padding': 256,
+    'padding': 0,
     'tolerance': 70
   });
 
