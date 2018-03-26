f4c4008e2f0cae1efb0faea4725a806281ecea4a
camilne <cameronm213@gmail.com>
2018-03-26 15:51:58 -0600

    Added staff verification for scraper

--- scraper/views.py
+++ scraper/views.py
@@ -3,13 +3,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.utils.html import escape
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.decorators import login_required
 from urllib.error import URLError
 from scraper import parse
 from .errors import UnknownWebsiteError, RecipeParsingError
 
 # Create your views here.
+@login_required
 def scrape(request):
     """Return a json object from a given url. URL is specified by '?url=' parameter."""
+    if not request.user.is_staff:
+        raise PermissionDenied
+
     site_url = escape(request.GET.get('url', ''))
 
     results = {'valid': 'false', 'source_url': site_url}
