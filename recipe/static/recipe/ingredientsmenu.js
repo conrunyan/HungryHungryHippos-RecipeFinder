ffcda63e4b49ee2d6d2c2d71c848b100506e4427
Aylee <ayleeandersen@gmail.com>
2018-03-27 15:30:22 -0600

    When an ingredient is selected or unselected, the list of ingredients is updated

--- recipe/static/recipe/ingredientsmenu.js
+++ recipe/static/recipe/ingredientsmenu.js
@@ -3,6 +3,7 @@
 $(document).ready(function() {
   let COLLAPSED_SRC = "http://icons.iconarchive.com/icons/awicons/vista-artistic/128/add-icon.png";
   let EXPANDED_SRC = "https://cdn1.iconfinder.com/data/icons/basic-ui-elements-color/700/07_minus-128.png";
+  var listOfIngredients = [];
 
   updateImage($(".update-on-expand"), COLLAPSED_SRC);
 
@@ -14,7 +15,20 @@ $(document).ready(function() {
     updateImage($(this), COLLAPSED_SRC);
   });
 
+  $(".update-list").change(function() {
+    addToList($(this));
+  });
+
   function updateImage(div, src) {
     div.siblings().find(".update-image").attr("src", src);
   };
+
+  function addToList(element) {
+    if (element.prop("checked")) {
+      listOfIngredients.push(element.prop("value"));
+    } else {
+      var index = listOfIngredients.indexOf(element.prop("value"));
+      if (index !== -1) listOfIngredients.splice(index, 1);
+    }
+  }
 });
