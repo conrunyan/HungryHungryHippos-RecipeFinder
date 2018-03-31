cd13a9ddadc33f765e6d5482adc5e4cb5c0737ea
connor <connor.runyan@aggiemail.usu.edu>
2018-03-30 23:39:54 -0600

    Continuing to work on the next10 class and its methods. Added outline for class and get next indices method

--- recipe/static/recipe/next10ingredients.js
+++ recipe/static/recipe/next10ingredients.js
@@ -6,13 +6,53 @@
 function Next10(recipes) {
     // public
     this.getNext10Recs=function() {
-        alert('hello');
+        
+        // check if incrementing another 10 recipes will exceed the bounds of
+        // the recipe list
+        
+        
     };
     this.getNumRecs=function() {
         return num_recs;
     }
-    // private
+    // private attributes
     var rec_start_idx = 0;
+    var rec_end_idx = 9;
     var num_recs = recipes.results.length;
+    // private methods
+
+    var getNextIndices=function() {
+        // Returns a two element array, the first element being
+        // the starting index, and second being the ending index of the
+        // next recipes to grab.
+
+        // increment indices
+        var next_start_idx = rec_start_idx + 1;
+        var next_end_idx = rec_end_idx + 10;
+        var next_indices = [0, 0];
+        const var START = 0;
+        const var END = 1;
+        // check if last starting index was the last item in the 
+        // recipes list
+        if (rec_start_idx == num_recs - 1)
+        {
+            // if yes, return the same index it started with
+            next_start_idx = rec_start_idx;
+            next_end_idx = rec_start_idx;
+            next_indices[START] = next_start_idx;
+            next_indices[END] = next_end_idx;
+        }
+        // else if the ending index is greater than rec_nums, return rec_nums
+        // as the ending index, and next_start_idx as the staring index
+        // TODO: add else if with above logic here
+        
+        // else, return next_end_idx as ending index, and next_start_idx as starting index
+        // TDOO: add else to capture above logic
+
+        // store used indices in the rec_start_idx and rec_end_idx values 
+        // TODO: store used 
+
+        return next_indices;
+    }
 }
 
