e0dca473bcc4262b62f854e655e331c0fba34489
connor <connor.runyan@aggiemail.usu.edu>
2018-03-31 23:57:38 -0600

    Continuing to work on the next10 function

--- recipe/static/recipe/next10ingredients.js
+++ recipe/static/recipe/next10ingredients.js
@@ -3,7 +3,7 @@
 // and interface with Kayson's "addRecipesToPage" function, incrementing on the
 // next 10 available recipes (if applicable).
 
-function Next10(recipes) {
+func = function Next10(recipes) {
     // public
     this.getNext10Recs=function() {
         
@@ -21,36 +21,48 @@ function Next10(recipes) {
     var num_recs = recipes.results.length;
     // private methods
 
-    var getNextIndices=function() {
+    this.getNextIndices=function() {
         // Returns a two element array, the first element being
         // the starting index, and second being the ending index of the
         // next recipes to grab.
 
         // increment indices
-        var next_start_idx = rec_start_idx + 1;
+        var next_start_idx = rec_start_idx + 10;
         var next_end_idx = rec_end_idx + 10;
         var next_indices = [0, 0];
-        const var START = 0;
-        const var END = 1;
+        var START = 0;
+        var END = 1;
         // check if last starting index was the last item in the 
         // recipes list
         if (rec_start_idx == num_recs - 1)
         {
             // if yes, return the same index it started with
-            next_start_idx = rec_start_idx;
-            next_end_idx = rec_start_idx;
+            next_indices[START] = rec_start_idx;
+            next_indices[END] = rec_start_idx;
+        }
+        // else if the ending index is greater than rec_nums, return the position of 
+            //rec_nums-1 as the ending index, and rec_start_idx as the staring index
+        // TODO: add else if with above logic here
+        else if (rec_end_idx > num_recs)
+        {
+            next_end_idx = num_recs -1;
             next_indices[START] = next_start_idx;
             next_indices[END] = next_end_idx;
         }
-        // else if the ending index is greater than rec_nums, return rec_nums
-        // as the ending index, and next_start_idx as the staring index
-        // TODO: add else if with above logic here
-        
         // else, return next_end_idx as ending index, and next_start_idx as starting index
         // TDOO: add else to capture above logic
+        else
+        {
+            next_indices[START] = next_start_idx;
+            next_indices[END] = next_end_idx;
+        }
 
         // store used indices in the rec_start_idx and rec_end_idx values 
         // TODO: store used 
+        rec_start_idx += 10;
+        rec_end_idx += 10;
+        rec_start_idx = next_indices[START];
+        rec_end_idx = next_indices[END];
 
         return next_indices;
     }
