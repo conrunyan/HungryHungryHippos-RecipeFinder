f806cfc264e963e68f745c10c92d584ba8170c52
connor <connor.runyan@aggiemail.usu.edu>
2018-04-02 15:39:54 -0600

    re-wrote next10ingredients thing... Classes just weren't working :/

--- recipe/static/recipe/next10ingredients.js
+++ recipe/static/recipe/next10ingredients.js
@@ -3,50 +3,34 @@
 // and interface with Kayson's "addRecipesToPage" function, incrementing on the
 // next 10 available recipes (if applicable)
 
-function Next10(recipes) {
-    // Constructor stuff
+let rec_start_idx = 0;
+let rec_end_idx = 9;
+let num_recs = 0;
+let START = 0;
+let END = 1;
+let cur_recipes;
+let cur_button;
 
-    // add button if recipes count is greater than 10, and remove it if 
-    // the last recipe in the list was displayed
-
-    // public
-    this.getNext10Recs=function() {
+function getNext10Recs(found_recipes) {
         // check if incrementing another 10 recipes will exceed the bounds of
         // the recipe list
-        var START = 0;
-        var END = 1;
-        var indices = getNextIndices();
-        // get starting and ending recipes
-        var start_idx = indices[START];
-        var end_idx = indices[END];
-        addRecipesToPage(found_recipes);
-    };
-    this.getNumRecs=function() {
-        return num_recs;
-    };
-    this.getLastIdx=function() {
-        return rec_end_idx;
-    };
-    this.getStartIdx=function() {
-        return rec_start_idx;
-    };
-    // private attributes
-    var found_recipes = recipes;
-    var rec_start_idx = 0;
-    var rec_end_idx = 9;
-    var num_recs = recipes.length;
-    // private methods
-    var getNextIndices=function() {
+        let indices = getNextIndices();
+        // get starting and ending recipes as an array
+        let start_idx = indices[START];
+        let end_idx = indices[END];
+        // call Kayson's function
+        addRecipesToPage(found_recipes, start_idx, end_idx);
+};
+
+function getNextIndices() {
         // Returns a two element array, the first element being
         // the starting index, and second being the ending index of the
         // next recipes to grab.
 
         // increment indices
-        var next_start_idx = rec_start_idx + 10;
-        var next_end_idx = rec_end_idx + 10;
-        var next_indices = [0, 0];
-        var START = 0;
-        var END = 1;
+        let next_start_idx = rec_start_idx + 10;
+        let next_end_idx = rec_end_idx + 10;
+        let next_indices = [0, 0];
         // check if last ending index was the last item in the 
         // recipes list
         if (rec_end_idx == num_recs - 1)
@@ -77,44 +61,48 @@ function Next10(recipes) {
         rec_end_idx = next_indices[END];
 
         // show or hide button based on recipe status
-        addRemoveButton(num_recs, rec_end_idx);
+        addRemoveButton();
 
         return next_indices;
-    };
-}
+};
 
-function addRemoveButton(num_recs, rec_end_idx) {
+function addRemoveButton() {
         // if number of recipes is less than 10, or the last recipe displayed was the 
         // last recipe in the list, don't display the button
-        var button = document.getElementById('next_10_button');
         
-        //if (num_recs < 0 || rec_end_idx == num_recs - 1)
-        //{
-        //    button.style = "display: none";
-        //}
-        // otherwise, display it
-        //else
-        //{
-            button.style = "";
-        //}
+        if (num_recs < 0 || rec_end_idx == num_recs - 1)
+        {
+            cur_button.style = "display: none";
+        }
+         // otherwise display it
+        else
+        {
+            cur_button.style = "";
+        }
     };
 
+// TODO: Write function to kill the button upon new search
+
 function runNext10(recipes){
-    var next10 = new Next10(recipes);
+
+    num_recs = recipes.length;
+    cur_recipes = recipes;
 
     // create button to add, if needed
-    var button_div = document.getElementById('next_10_button_div');
-    alert(button_div.className);
-    var button = document.createElement('button');
-    button.classList = 'btn btn-dark';
-    button.type = 'button';
-    button.id = 'next_10_button';
-    button.textContent = "Next 10";
+    let button_div = document.getElementById('next_10_button_div');
+    let next_button = document.createElement('button');
+    next_button.classList = 'btn btn-dark';
+    next_button.type = 'button';
+    next_button.id = 'next_10_button';
+    next_button.textContent = "See More...";
     //button.style = 'display: none';
-    button.addEventListener('click', next10.getNext10Recs());
-    button_div.appendChild(button);
+    next_button.setAttribute('onclick', 'getNext10Recs(cur_recipes);');
+    button_div.appendChild(next_button);
+    cur_button = next_button;
+    console.log(next_button);
+    // add button to next_10_button_div
+    
 
-    alert(button_div.children);
     // determine if button is shown or not. 
-    //addRemoveButton(next10.getNumRecs, next10.getLastIdx);
+    addRemoveButton();
 }
