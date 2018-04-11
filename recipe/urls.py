42441d8a73354731f2f17fa810599bde2fc59bb6
connor <connor.runyan@aggiemail.usu.edu>
2018-04-11 12:31:43 -0600

    Added submit for public button, and started working on the view to actually submit it.

--- recipe/urls.py
+++ recipe/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     url(r'^delete_recipe/(?P<id>[0-9]+)/', views.delete_recipe_view, name='delete_recipe_view'),
     url(r'^recipe_full_view/(?P<id>[0-9]+)/', views.recipe_full_view, name='recipe_full_view'),
     url(r'^get_recipes/$', views.get_recipes, name='get_recipes'),
+    url(r'^submit_for_public/(?P<id>[0-9]+)/$', views.submit_for_public, name='submit_for_public'),
 ]
