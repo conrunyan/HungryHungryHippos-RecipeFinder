778b118968e9bbfec1b63c6792ec769517b2e6f5
Kayson <KaysonBarwick@hotmail.com>
2018-04-18 20:38:45 -0600

    Resolves #37 changed add recipe form to accept only minutes instead of minutes and hours.

--- recipe/forms.py
+++ recipe/forms.py
@@ -8,9 +8,6 @@ import re
 
 
 class RecipeForm(forms.ModelForm):
-    time = forms.CharField(widget=forms.TextInput(attrs={
-                           'class': 'form-control', 'placeholder': 'Time (Hours:Minutes)'}))
-
     class Meta:
         model = Recipe
         fields = ('title', 'summary', 'difficulty',
@@ -19,34 +16,10 @@ class RecipeForm(forms.ModelForm):
             'title':        forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Recipe Name'}),
             'summary':      forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Description'}),
             'difficulty':   forms.Select(attrs={'class': 'form-control', 'placeholder': 'Difficulty'}),
+            'time':         forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Time (Minutes)'}),
             'image_url':    forms.URLInput(attrs={'class': 'form-control', 'placeholder': 'Image URL'}),
             'instructions': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Instructions'}),
         }
-
-    def clean_time(self):
-        time = self.cleaned_data['time']
-        time_re = re.compile(r'(\d*):?(\d*)')
-        hourmin = time_re.match(time)
-
-        hours = 0
-        mins = 0
-        if hourmin.group(1) != "":
-            hours = int(hourmin.group(1))
-        if hourmin.group(2) != "":
-            mins = int(hourmin.group(2))
-        time = hours * 60 + mins
-        if(time == 0):
-            vaild = False
-            self.add_error(
-                'time', 'You must enter a valid non-zero time! (Hours:Minutes)')
-#            ValidationError(_('Invalid value'), code='invalid')
-
-        return time
-
-    def is_valid(self):
-        valid = super(RecipeForm, self).is_valid()
-
-        return valid
 
 
 class RecipeIngredientForm(forms.ModelForm):
