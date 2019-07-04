6dc04c3cd2f3c464599fd86ff95124afe02938ed
Connor Runyan <connor.runyan@aggiemail.usu.edu>
2019-07-04 16:38:49 -0600

    Adjusted login info

--- hhh/settings.py
+++ hhh/settings.py
@@ -20,7 +20,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'noreply.hhhippo@gmail.com'
-EMAIL_HOST_PASSWORD = 'Hungry4evr'
+EMAIL_HOST_PASSWORD = 'INVALID_PASSWORD'
 EMAIL_USE_TLS = True
 
 
